3cb9549ef120752c906e1953d6f7251bc55f2fae
= <josephag1978@uds.edu.gh>
2023-11-08 17:50:12 +0100

    done

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate  # Import the Migrate class
+from flask_migrate import Migrate
+from flask_mail import Mail
 
 db = SQLAlchemy()
-
-# Initialize Migrate
 migrate = Migrate()
+mail = Mail()  # Create the Mail instance
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
-    migrate.init_app(app, db)  # Initialize Flask-Migrate with your app and db
+    migrate.init_app(app, db)
+    mail.init_app(app)  # Initialize Flask-Mail with your app
 
     with app.app_context():
         # Import models and create database tables if necessary
