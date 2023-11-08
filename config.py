8d14d5945c8a418e8ac37a79a5876eab1a9a1ac4
= <josephag1978@uds.edu.gh>
2023-11-08 17:13:46 +0100

    mail function added to groupontroller

--- config.py
+++ config.py
@@ -8,8 +8,18 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your-secret-key'  # Used to add additional security for Flask sessions
 
-BASE_URL = 'http://localhost:5000'  # Base URL for the application
+    BASE_URL = 'http://localhost:5000'  # Base URL for the application
 
-API_KEY = 'AIzaSyAJ7BoV6AdTat8DK14e2nFCAqOHTl1asqM'
-UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
-BANNER_UPLOAD_FOLDER = os.path.join(basedir, 'banner_uploads')
+    API_KEY = 'AIzaSyAJ7BoV6AdTat8DK14e2nFCAqOHTl1asqM'
+
+    UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+    BANNER_UPLOAD_FOLDER = os.path.join(basedir, 'banner_uploads')
+
+    
+    MAIL_SERVER = 'smtp.example.com'  # Replace with your mail server
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'your-email@example.com'  # Replace with your email
+    MAIL_PASSWORD = 'your-password'  # Replace with your password
+    MAIL_DEFAULT_SENDER = 'your-email@example.com'  # Replace with your email
+
