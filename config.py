d0ebeddf973f7dc3d3ebd557a85bd535ea0ccfb6
= <josephag1978@uds.edu.gh>
2023-11-08 21:51:42 +0100

    updated

--- config.py
+++ config.py
@@ -2,15 +2,18 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+API_KEY = 'AIzaSyAJ7BoV6AdTat8DK14e2nFCAqOHTl1asqM'
+
+BASE_URL = 'http://localhost:5000'  # Base URL for the application
+
 class Config:
     # SQLite database file will be located at the project's root directory
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'peer_connect.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your-secret-key'  # Used to add additional security for Flask sessions
 
-    BASE_URL = 'http://localhost:5000'  # Base URL for the application
+   
 
-    API_KEY = 'AIzaSyAJ7BoV6AdTat8DK14e2nFCAqOHTl1asqM'
 
     UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
     BANNER_UPLOAD_FOLDER = os.path.join(basedir, 'banner_uploads')
